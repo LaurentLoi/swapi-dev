c1732abc198a47ba858f4a3f5511efca0fc41be3
Laurent Loiseau <lt.loiseau@gmail.com>
2022-03-05 19:41:05 +0100

    improved ICommand

--- src/cli/commands/swapi-base.command.ts
+++ src/cli/commands/swapi-base.command.ts
@@ -2,6 +2,15 @@ import { ICommand } from '../models/command.model';
 
 export const swapiBaseCommand: ICommand = {
     command: '[Star Wars movie number from 1 to 7]',
-    description: 'Prints the total diameter of the planets with mountains and water surface for a given movie number.',
+    description: 'Prints the total diameter of the planets with mountains and water surfaces for a given movie number.',
     optional: false,
+    exampleUsage: '6',
+    optionalCommands: [
+        {
+            command: 'w',
+            description: '',
+            optional: true,
+            exampleUsage: '6 w',
+        },
+    ],
 };
--- src/cli/models/command.model.ts
+++ src/cli/models/command.model.ts
@@ -2,4 +2,6 @@ export interface ICommand {
     command: string;
     description: string;
     optional: boolean;
+    exampleUsage: string;
+    optionalCommands?: ICommand[];
 }

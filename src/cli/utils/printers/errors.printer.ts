0d44414ed2fa47f6e14a3454e7f17bf32f8f1118
Laurent Loiseau <lt.loiseau@gmail.com>
2022-03-05 20:01:19 +0100

    added tabs & colors in errors.printer.ts

--- src/cli/utils/printers/errors.printer.ts
+++ src/cli/utils/printers/errors.printer.ts
@@ -4,17 +4,21 @@ import { Service } from 'typedi';
 import { PrettyPrinter } from './pretty.printer';
 import { LogLevelsEnum } from '../../enums/log-levels.enum';
 import { ICliError } from '../../models/error.model';
+import { CliColorsEnum } from '../../enums/cli-colors.enum';
 
 @Service()
 export class ErrorsPrinter {
+
+    private colors = CliColorsEnum;
+
     constructor(private prettyPrinter: PrettyPrinter) {}
 
     public errorPrinter(error: ICliError): void {
         this.baseErrorPrinter();
 
-        this.prettyPrinter.prettyPrint(error.error, false, LogLevelsEnum.ERROR);
-        this.prettyPrinter.prettyPrint(error.description, false, LogLevelsEnum.ERROR);
-        this.prettyPrinter.prettyPrint(error.resolution, true, LogLevelsEnum.ERROR);
+        this.prettyPrinter.prettyPrint(error.error, false, LogLevelsEnum.ERROR, 1);
+        this.prettyPrinter.prettyPrint(error.description, false, LogLevelsEnum.ERROR, 1);
+        this.prettyPrinter.prettyPrint(error.resolution, true, LogLevelsEnum.ERROR, 1);
 
         if (error.addBaseHelper) {
             this.baseUsagePrinter();
@@ -23,24 +27,28 @@ export class ErrorsPrinter {
 
     private baseErrorPrinter(): void {
         this.prettyPrinter.emptyLinePrinter();
-        this.prettyPrinter.prettyPrint('Sorry, an error occurred: ', true, LogLevelsEnum.ERROR);
+        this.prettyPrinter.prettyPrint('Sorry, an error occurred: ', false, LogLevelsEnum.ERROR);
     }
 
     private baseUsagePrinter(): void {
-        this.prettyPrinter.prettyPrint('Here are the base cli commands: ', true); // add app desc
+        this.prettyPrinter.prettyPrint('Here are the base cli commands: ', true); // todo add app desc & title
         commandsContainer.forEach((command: ICommand, index: number) => {
             this.commandPrinter(command, index + 1);
             command.optionalCommands?.forEach((subCommand: ICommand, subIndex: number) => {
-                this.commandPrinter(subCommand, (index + 1) + '.' + (subIndex + 1));
+                this.commandPrinter(subCommand, (index + 1) + '.' + (subIndex + 1), 1);
             });
         });
     }
 
-    private commandPrinter(command: ICommand, startLine: number | string): void { // todo add colors
-        this.prettyPrinter.prettyPrint(`${ startLine }: ${ command.command }`);
-        this.prettyPrinter.prettyPrint(`   Description: ${ command.description }`);
-        this.prettyPrinter.prettyPrint(`   Optional command: ${ command.optional }`);
-        this.prettyPrinter.prettyPrint(`   Eg.: npm start ${ command.exampleUsage }`, true);
+    private commandPrinter(command: ICommand, startLine: number | string, tab?: number): void {
+        this.prettyPrinter.prettyPrint(`${ startLine }: ${ this.colors.GREEN }${ command.command }${ this.colors.RESET }`, false,
+            LogLevelsEnum.INFO, tab);
+        this.prettyPrinter.prettyPrint(`     Description: ${ this.colors.CYAN }${ command.description }${ this.colors.RESET }`, false,
+            LogLevelsEnum.INFO, tab);
+        this.prettyPrinter.prettyPrint(`     Optional command: ${ this.colors.YELLOW }${ command.optional }${ this.colors.RESET }`, false,
+            LogLevelsEnum.INFO, tab);
+        this.prettyPrinter.prettyPrint(`     Eg.: ${ this.colors.GREEN }npm start ${ command.exampleUsage }${ this.colors.RESET }`, true,
+            LogLevelsEnum.INFO, tab);
     }
 
 }

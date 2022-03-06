9f7c34cc45c09f41fef2a122de20e49d44044d93
Laurent Loiseau <lt.loiseau@gmail.com>
2022-03-06 19:11:25 +0100

    added base exercise resolution in swapi-base.service.ts (to be cleaned)

--- src/cli/services/swapi-base.service.ts
+++ src/cli/services/swapi-base.service.ts
@@ -33,22 +33,36 @@ export class SwapiBaseService {
                 });
             } else {
                 this.film$.subscribe(async (film: IFilm) => {
-                    this.prettyPrinter.prettyPrint(`Found film[${ +(params[0]) }] title: `, false, LogLevelsEnum.ALERT, 1);
-                    this.prettyPrinter.prettyPrint(film.title, false, LogLevelsEnum.FANCY, 2);
-                    this.prettyPrinter.prettyPrint(`Found film[${ +(params[0]) }] planet[0]: `, false, LogLevelsEnum.ALERT, 1);
-                    this.prettyPrinter.prettyPrint(film.planets[0], true, LogLevelsEnum.FANCY, 2);
+                    this.prettyPrinter.prettyPrint(`Film ${ +(params[0]) } title: `, false, LogLevelsEnum.SUCCESS, 1);
+                    this.prettyPrinter.prettyPrint(film.title, true, LogLevelsEnum.FANCY, 2);
+                    this.prettyPrinter.prettyPrint(`Number of planets: `, false, LogLevelsEnum.SUCCESS, 1);
+                    this.prettyPrinter.prettyPrint(film.planets.length.toString(), true, LogLevelsEnum.FANCY, 2);
 
                     const currentFilmPlanetIds: number[] = urlParser(film.planets);
                     await this.planetHttpService.getFilmPlanetsById(currentFilmPlanetIds).then((planets: IPlanet[]) => {
-                        this.prettyPrinter.prettyPrint('my planets: ', true, LogLevelsEnum.ALERT, 1);
+                        this.prettyPrinter.prettyPrint('Planets name: ', true, LogLevelsEnum.SUCCESS, 1);
                         planets.forEach((planet: IPlanet) => {
                             this.prettyPrinter.prettyPrint(planet.name, false, LogLevelsEnum.FANCY, 2);
                         });
+                        this.prettyPrinter.emptyLinePrinter();
+
+                        this.prettyPrinter.prettyPrint('Planets with water surfaces and mountains: ', true, LogLevelsEnum.SUCCESS,
+                            1);
+
+                        let totalDiameter = 0;
+                        planets.filter((planet: IPlanet) => planet.terrain.toLowerCase().includes('mountains') && +planet.surface_water > 0)
+                            .forEach((planet: IPlanet) => {
+                                this.prettyPrinter.prettyPrint(`Planet: ${ planet.name }`, false, LogLevelsEnum.FANCY, 2);
+                                this.prettyPrinter.prettyPrint(`Diameter: ${ planet.diameter }`, true, LogLevelsEnum.FANCY, 2);
+                                totalDiameter += +planet.diameter;
+                            });
+                        this.prettyPrinter.prettyPrint(`Total diameter of planets with mountains and water surfaces: `, false,
+                            LogLevelsEnum.SUCCESS, 2);
+                        this.prettyPrinter.prettyPrint(`${ totalDiameter.toString() }`, true, LogLevelsEnum.FANCY, 3);
                         this.prettyPrinter.cliDelimiter('end');
                     });
                 });
             }
         });
     }
-
 }
